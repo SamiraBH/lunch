43fad7886ad07c0c680a61f220f6c6b373e09c42
SamiraBani <banihassan.s@gmail.com>
2021-02-01 23:32:18 +0000

    Set up alunch time in bootcamp

--- lunch.js
+++ lunch.js
@@ -13,15 +13,28 @@
  * in minutes.
  */
 
-const whatToDoForLunch = function(hungry, availableTime) {
-  console.log("I don't know what to do!");
-}
-
+// const whatToDoForLunch = function(hungry, availableTime) {
+//   console.log("I don't know what to do!");
+// };
 
 /*
  * This is some test runner code that's simply calling our whatToDoForLunch function
  * defined above to verify we're making the right decisions. Do not modify it!
  */
+
+const whatToDoForLunch = function(hungry, availableTime) {
+  if (hungry) {
+    if (availableTime <= 20) {
+      console.log("Pickup lunch and eat back at the lab");
+    } else if (availableTime > 20 && availableTime <= 30) {
+      console.log("You deserve a break, try a place in Gastown.");
+    } else if (availableTime > 30) {
+      console.log("Grab quick lunch and get back to work.");
+    }
+  } else {
+    console.log("Work until you're hungry.");
+  }
+};
 
 console.log("I'm hungry and I have 20 minutes for lunch.");
 whatToDoForLunch(true, 20);
